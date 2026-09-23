eff700dd3dafe5c971354ff17332102d712b88b4
IlConteDiGello <tomilari08@gmail.com>
2026-09-23 10:31:38 +0200

    non sto capendo una mazza

--- src/main/java/com/mycompany/matchmaking/client.java
+++ src/main/java/com/mycompany/matchmaking/client.java
@@ -1,4 +1,18 @@
 
+/*
+SERVER DI MATCH MAKING  
+Il server dovrà essere in ascolto su una porta TCP nota ed accettare le richieste di match-making dei vari  client. Nei parametri di configurazione il server dovrà avere necessariamente:  
+• numero di giocatori per gruppo (ngg)  
+• differenza massima di livello tra i giocatori (dml)  
+Quando un giocatore si connette al server, questi dovrà registrarlo in un pool di giocatori in attesa di essere  abbinati e metterlo in attesa.  
+Non appena risulti possibile creare un gruppo di ngg giocatori, il server dovrà risvegliare i rispettivi client  comunicando loro la lista dei propri compagni.  
+Tale lista dovrà contenere almeno queste informazioni (per ogni giocatore):  
+• nickname del giocatore  
+• indirizzo ip della suo client  
+• livello del giocatore  
+Nota: il server dovrà lavorare a ciclo continuo. Una volta avviato, solo l’amministratore potrà decidere la  sua terminazione.  
+
+ */
 package com.mycompany.matchmaking;
 
 import java.io.BufferedReader;
@@ -10,6 +24,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class client {
+
+    String nickName;
+    int livello;
 
     /**
      * @param args the command line arguments
